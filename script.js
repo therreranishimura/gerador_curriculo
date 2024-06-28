7f8fbbf23a8639592eb6f9a70efc59432493e2a9
therreranishimura <therreranishimura@gmail.com>
2024-06-28 12:24:22 -0300

    adição página de escolaridade

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const form = document.getElementById("form");
 const username = document.getElementById("username")
+const data = document.getElementById("data")
 const email = document.getElementById("email")
 const telefone = document.getElementById("telefone")
+const cep = document.getElementById("cep")
 
 form.addEventListener ("submit", (event) => {
     event.preventDefault();
@@ -13,6 +15,10 @@ username.addEventListener("blur", () => {
     checkInputUsername();
 })   
 
+data.addEventListener("blur", () => {
+    checkInputData();
+})  
+
 email.addEventListener("blur", () => {
     checkInputEmail();
 })
@@ -21,13 +27,28 @@ telefone.addEventListener("blur", () => {
     checkInputTelefone();
 })
 
+cep.addEventListener("blur", () => {
+    checkInputCep();
+})
+
 function checkInputUsername(){
     const usernameValue = username.value;
 
     if (usernameValue === "") {
-        errorInput(username, "Esse campo é obrigatório")
+        errorInput(username, "Campo obrigatório")
     }else{ 
         const formItem = username.parentElement;
+        formItem.className = "form-content"
+    }
+}
+
+function checkInputData(){
+    const dataValue = data.value;
+
+    if (dataValue === "") {
+        errorInput(data, "Campo obrigatório")
+    }else{ 
+        const formItem = data.parentElement;
         formItem.className = "form-content"
     }
 }
@@ -36,7 +57,7 @@ function checkInputEmail(){
     const emailValue = email.value;
 
     if (emailValue === "") {
-        errorInput(email, "Esse campo é obrigatório")
+        errorInput(email, "Digite um email válido")
     }else{ 
         const formItem = email.parentElement;
         formItem.className = "form-content"
@@ -47,9 +68,20 @@ function checkInputTelefone(){
     const telefoneValue = telefone.value;
 
     if (telefoneValue === "") {
-        errorInput(telefone, "Esse campo é obrigatório")
+        errorInput(telefone, "Digite um número de telefone válido")
     }else{ 
         const formItem = telefone.parentElement;
+        formItem.className = "form-content"
+    }
+}
+
+function checkInputCep(){
+    const cepValue = cep.value;
+
+    if (cepValue === "") {
+        errorInput(cep, "Campo obrigatório")
+    }else{ 
+        const formItem = cep.parentElement;
         formItem.className = "form-content"
     }
 }
@@ -58,6 +90,8 @@ function checkForm(){
     checkInputUsername();
     checkInputEmail();
     checkInputTelefone();
+    checkInputCep();
+    checkInputData();
 
     const formItems = form.querySelectorAll(".form-content")
     const isValid = [...formItems].every ( (item) => {
